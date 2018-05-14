c3ce7c3752f062058e915add9f74a1555af0b496
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-14 17:11:52 +0200

    Building the Recipe Model - PART I - Moving proxy and key to single config file to be resusable

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key, proxy } from '../config'
 
 export default class Search {
     constructor(query) {
@@ -7,8 +8,7 @@ export default class Search {
 
     // async method 
     async getResults(query) {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = 'f7c0cce3d19eae6cad084e582751ce9f';
+
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
