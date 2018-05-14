bd7b1fcea2ca8d7de6dd43f59899fd4a35615682
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-14 11:40:48 +0200

    Building the SEARCH MODEL

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
-// f7c0cce3d19eae6cad084e582751ce9f
-// http://food2fork.com/api/search
+import Search from './models/Search';
 
-
-import axios from 'axios';
-
-// How we handle promises with async await
-// SO the result of the value that we start here will then be saved into the res variable
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = 'f7c0cce3d19eae6cad084e582751ce9f';
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-}
-
-getResults('pizza'); 
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
