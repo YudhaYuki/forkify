2991bea3324341f84af5caf14d8f9494c8c203f8
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-14 10:52:09 +0200

    Making First API Calls - Using Proxy

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,11 @@ import axios from 'axios';
 // How we handle promises with async await
 // SO the result of the value that we start here will then be saved into the res variable
 async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
     const key = 'f7c0cce3d19eae6cad084e582751ce9f';
-    const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(res);
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
 }
 
-getResults();
+getResults('pizza'); 
